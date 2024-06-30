53d7afb6424a841674e7e7a824bd3c57ee4b1466
nick/ga <158477658+SosiskaKillerS@users.noreply.github.com>
2024-06-30 14:23:42 +0300

    some code

--- src/Player.gd
+++ src/Player.gd
@@ -1,11 +1,22 @@
 extends CharacterBody2D
 
-@export var speed = 500
+var speed : int
+var screen_size : Vector2
+
+func _ready():
+	speed = 25
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
-
+	var input_dir = Input.get_vector("left","right","up", "down")
+	velocity = input_dir * speed
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	var mouse = get_local_mouse_position()
+	var angle = snappedf(mouse.angle(), PI/4) / (PI/4)
+	
+	if velocity.length() != 0:
+		$AnimationPlayer.play()
+	else:
+		$AnimationPlayer.stop()
+		
